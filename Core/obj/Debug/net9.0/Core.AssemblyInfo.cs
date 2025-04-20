a0412b64df88a80af95c07d34f69d5671b69dc49
mberkayakardev <mberkayakar@outlook.com>
2025-04-20 11:50:52 +0300

    format oncesi

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d422c8c30d69cb03e5bd2e65c8fa12199ee2f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a163ba752a130fd8475530c5581ad9976bae44b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
